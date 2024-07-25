662557981c53d3dcb4136d023436a4c26c856cdb
Tenma <tenma@osxt.com>
2024-07-25 15:58:13 +0200

    fix

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ const User = require("./model/user");
 dotenv.config();
 
 const mongoDB = process.env.MONGODB_URL;
+
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
