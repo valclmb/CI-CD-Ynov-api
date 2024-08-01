2c6fa29fc7ef32f79189dbc8420119d5dbd98ae8
Tenma <tenma@osxt.com>
2024-08-01 12:23:36 +0200

    add e2e post

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,23 @@
+const { postUsers } = require("../../src/api");
+
 describe("GET /users", () => {
   it("gets a list of users", () => {
     cy.request("GET", "/users").then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.users).length.to.be.greaterThan(1);
+    });
+  });
+});
+
+describe("POST /users", () => {
+  it("creates a new user", () => {
+    cy.request("POST", "/users", {
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@email.fr",
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body.message).to.eq("User created");
     });
   });
 });
